b631b80a7bac14e5402ade6f1287db2d6221f6a0
thadchet <sogood_boss@hotmail.com>
2020-11-11 11:26:55 +0700

    fix server

--- server/index.js
+++ server/index.js
@@ -52,6 +52,6 @@ app.use("/api/posts", passport.authenticate("jwt", { session: false }), posts);
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
 app.use("/",(req,res)=>{
-  res.send("Hello Computer security!!")
+  res.send("Hello Computer security !!!!")
 })
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
