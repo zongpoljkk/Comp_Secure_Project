1278fcc256b2747c68ea852db39ac7c62778d373
thadchet <sogood_boss@hotmail.com>
2020-11-11 11:07:25 +0700

    add Docker

--- server/index.js
+++ server/index.js
@@ -50,4 +50,8 @@ app.use("/api/users", users);
 app.use("/api/posts", passport.authenticate("jwt", { session: false }), posts);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+
+app.use("/",(req,res)=>{
+  res.send("Hello Computer security!!")
+})
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
